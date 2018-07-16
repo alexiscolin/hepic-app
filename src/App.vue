b93af4e25d6dfda58c95e3a3d9366e15ae0c3dd8
Alexis Colin <nesquimo@gmail.com>
2018-07-16 19:14:20 +0200

    Ajout du footer

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <spriteSvg/>
     <Header/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import spriteSvg from './components/layout/svg/Svg';
 import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 
 export default {
   name: 'App',
   components: {
     spriteSvg,
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/layout/footer/Footer.vue
@@ -0,0 +1,3 @@
+<template src="./template.pug" lang="pug"></template>
+<script src="./index.js"></script>
+<style src="./style.less" lang="less"></style>
