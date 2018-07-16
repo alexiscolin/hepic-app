d342ae5fb49598325a5e9a95ad37044ed31dfcb0
Alexis Colin <nesquimo@gmail.com>
2018-07-16 19:45:03 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@
 
 <script>
 import spriteSvg from './components/layout/svg/Svg';
-// import Header from './components/layout/header/Header';
+import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 
 export default {
   name: 'App',
   components: {
     spriteSvg,
-    // Header,
+    Header,
     Footer,
   },
 };
