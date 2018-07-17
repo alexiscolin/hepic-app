f3ba75c27b8fb4da6ec8e7593c900eebabfbd4cf
Alexis Colin <nesquimo@gmail.com>
2018-07-17 22:12:49 +0200

    Gestion des clotures prochaines

--- src/components/flux/tile/script.js
+++ src/components/flux/tile/script.js
@@ -1,4 +1,27 @@
+import dateFormat from '@/utils/date-format';
+
 export default {
   name: 'fluxTile',
   props: ['data'],
+  computed: {
+
+    // conv date début
+    getStartDate: function(){
+      const contestDateStart = new Date(this.data.dateStart);
+      return  dateFormat(contestDateStart);
+    },
+
+    // conv date fin
+    getEndDate: function(){
+      const contestDateEnd = new Date(this.data.dateEnd);
+      return dateFormat(contestDateEnd);
+    },
+
+    // verif fin de concours
+    closeSoon: function(){
+      const date = new Date();
+      date.setDate(date.getDate() + 10); // check à 10j
+      return date.getTime() >= new Date(this.data.dateEnd).getTime(); // UTC comparison
+    }
+  }
 }
