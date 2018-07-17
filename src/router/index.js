677f5716149f7281812ab88cded137cd06ef60b1
Alexis Colin <nesquimo@gmail.com>
2018-07-17 23:29:45 +0200

    Ajout de la route profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import tilesFlux from '@/components/flux';
+import userProfile from '@/components/profile';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Flux',
       component: tilesFlux,
+    },
+    {
+      path: '/user/:id',
+      name: 'User',
+      component: userProfile,
     },
   ],
 });
