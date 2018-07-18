7ba6ad81166a9555338a82e92bd9185a8e6711a0
Alexis Colin <nesquimo@gmail.com>
2018-07-18 18:40:28 +0200

    retirer props superficielles

--- src/components/image/script.js
+++ src/components/image/script.js
@@ -2,18 +2,9 @@ import config from './config.json';
 
 export default {
   name: 'displayImage',
-  // components: {
-  //   fluxTile,
-  //   fluxPodium
-  // },
-  data() {
-    return {
-      //image:
-    };
-  },
   computed: {
     media: function() {
-      let id = parseInt(this.$route.params.id);
+      const id = parseInt(this.$route.params.id);
       return config.pictures.find(item => item.id === id); // recup item verifiant l'id demandé
     }
   }
