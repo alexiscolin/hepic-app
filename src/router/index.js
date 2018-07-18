57c4297b9a9057e704e0bc7da8b12ae02afa529c
Alexis Colin <nesquimo@gmail.com>
2018-07-18 11:03:06 +0200

    Update de route statique vers user

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import tilesFlux from '@/components/flux';
-import userProfile from '@/components/profile';
+import userProfile from '@/components/user';
 
 Vue.use(Router);
 
@@ -14,7 +14,8 @@ export default new Router({
       component: tilesFlux,
     },
     {
-      path: '/user/:id',
+      // path: '/user/:id',
+      path: '/user',
       name: 'User',
       component: userProfile,
     },
