9b5fb1e68ae23d18a9a86b28bd03f78fb5e7fd80
Alexis Colin <nesquimo@gmail.com>
2018-07-19 09:50:36 +0200

    Reformater date dans contest

--- src/components/contest/script.js
+++ src/components/contest/script.js
@@ -1,4 +1,5 @@
 import config from './config.json';
+import dateFormat from '@/utils/date-format';
 
 export default {
   name: 'brandContest',
@@ -7,4 +8,16 @@ export default {
       contest: config.contest,
     };
   },
+  computed: {
+    getStartDate: function(){
+      const contestDateStart = new Date(this.contest.dateStart);
+      return  dateFormat(contestDateStart);
+    },
+
+    // conv date fin
+    getEndDate: function(){
+      const contestDateEnd = new Date(this.contest.dateEnd);
+      return dateFormat(contestDateEnd);
+    },
+  }
 }
