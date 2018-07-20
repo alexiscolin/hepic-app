26b6fdb31ee604d60c537a6cf038c341fe72c21d
Alexis Colin <nesquimo@gmail.com>
2018-07-21 00:56:42 +0200

    Lecture image et securisation type img

--- src/components/contest/upload/script.js
+++ src/components/contest/upload/script.js
@@ -6,11 +6,40 @@ export default {
   components: {
     contestLayout
   },
+  data(){
+    return {
+      picUploaded: false,
+      picture: null,
+      pictureRender: null,
+    }
+  },
   computed: {
     ...mapState({
       contest: function(state){
         return state.contestEntry.contest
       }
     })
+  },
+  methods: {
+    upload: function(e){
+      this.picture = e.target.files[0];
+
+      // validation type image
+      const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+      if(!allowedExtensions.exec(e.target.value)) return;
+
+      // confirmation state
+      this.picUploaded = true;
+
+      // lecture
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        this.pictureRender = reader.result;
+      }, false);
+      this.picture && reader.readAsDataURL(this.picture);
+    },
+    confirmUpload: function(){
+
+    }
   }
 }
