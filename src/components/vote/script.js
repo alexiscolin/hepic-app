6f3fa0ee1cc90a3b3191925569b7571001ad1b79
Alexis Colin <nesquimo@gmail.com>
2018-07-23 19:17:33 +0200

    Bouton de vote next et prev

--- src/components/vote/script.js
+++ src/components/vote/script.js
@@ -5,6 +5,22 @@ export default {
   data() {
     return {
       vote: config.vote,
+      index: 0,
+      indexMin: 0,
+      indexMax: config.vote.images.length - 1,
     };
+  },
+  computed: {
+    displayImage: function $displayImage() {
+      return config.vote.images[this.index].src;
+    },
+  },
+  methods: {
+    nextImg: function $nextImg() {
+      this.index = (this.index < this.indexMax) ? this.index + 1 : this.indexMax;
+    },
+    prevImg: function $prevImg() {
+      this.index = (this.index > this.indexMin) ? this.index - 1 : this.indexMin;
+    },
   },
 };
