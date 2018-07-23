21b6b9a7f991c6d7c491cfc52101305a6b3d8f4e
Alexis Colin <nesquimo@gmail.com>
2018-07-24 01:27:04 +0200

    Ajout fonction retrait header au scroll

--- src/components/layout/header/script.js
+++ src/components/layout/header/script.js
@@ -1,8 +1,31 @@
+import throttle from '@/utils/throttle';
+
 export default {
   name: 'appHeader',
   data() {
     return {
       msg: 'Hello tout le monde !',
+      hidden: false,
+      yPos: 0,
     };
+  },
+  methods: {
+    onScroll: throttle(function $throttleOnScroll() {
+      const down = this.yPos < window.pageYOffset;
+      if (down && !this.hidden) {
+        this.$el.classList.add('s--is-transY-100');
+        this.hidden = true;
+      } else if (!down && this.hidden) {
+        this.$el.classList.remove('s--is-transY-100');
+        this.hidden = false;
+      }
+      this.yPos = window.pageYOffset;
+    }, 500),
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll, false);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll, false);
   },
 };
---
+++ src/utils/throttle.js
@@ -0,0 +1,19 @@
+export default function (func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function $throttle(...args) {
+    const context = this;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
