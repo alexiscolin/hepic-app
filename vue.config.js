2680cd31ffac263c194c69bcc60b8952e6a4ac41
Alexis Colin <nesquimo@gmail.com>
2018-07-23 13:51:33 +0200

    Changement css pour gestion dégradé après arangement extractcss alphabet

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  css: {
-    sourceMap: true
-  },
+  css: undefined,
+
   configureWebpack: {
     resolve: {
       alias: {
         '@@': path.resolve('src/assets/style'),
       }
     }
-  }
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined
 }
