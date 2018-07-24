7683aa424b9843d15acf50913db75950c897c8af
Alexis Colin <nesquimo@gmail.com>
2018-07-24 19:48:48 +0200

    Changement route info perso sur settings

--- src/components/user/settings/script.js
+++ src/components/user/settings/script.js
@@ -5,12 +5,14 @@ export default {
   data() {
     return {
       data: config.data,
+      type: null,
       title: String,
-      settingsContent: null,
+      settingsContent: 'blabla from API',
     };
   },
   created: function $created() {
-    const vue = Array.from(this.data).find(el => el.type === this.$route.params.type);
+    this.type = this.$route.params.type;
+    const vue = Array.from(this.data).find(el => el.type === this.type);
     this.title = vue.title;
     this.desc = vue.desc || false;
   },
