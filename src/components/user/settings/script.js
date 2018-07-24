c98c0fe80b7f9beb771d8fab651adc0c1b9c39e0
Alexis Colin <nesquimo@gmail.com>
2018-07-24 20:47:11 +0200

    Style border reg

--- src/components/user/settings/script.js
+++ src/components/user/settings/script.js
@@ -5,13 +5,12 @@ export default {
   data() {
     return {
       data: config.data,
-      type: null,
+      type: this.$route.params.type,
       title: String,
       settingsContent: 'blabla from API',
     };
   },
   created: function $created() {
-    this.type = this.$route.params.type;
     const vue = Array.from(this.data).find(el => el.type === this.type);
     this.title = vue.title;
     this.desc = vue.desc || false;
