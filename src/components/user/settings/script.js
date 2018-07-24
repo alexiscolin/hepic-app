cf7dbd57d21025f7d9fa8e5b48cc79489aec8932
Alexis Colin <nesquimo@gmail.com>
2018-07-24 17:18:52 +0200

    Gestion de rendu titre settings

--- src/components/user/settings/script.js
+++ src/components/user/settings/script.js
@@ -1,3 +1,17 @@
+import config from './config.json';
+
 export default {
   name: 'settings',
+  data() {
+    return {
+      data: config.data,
+      title: String,
+      settingsContent: null,
+    };
+  },
+  created: function $created() {
+    const vue = Array.from(this.data).find(el => el.type === this.$route.params.type);
+    this.title = vue.title;
+    this.desc = vue.desc || false;
+  },
 };
