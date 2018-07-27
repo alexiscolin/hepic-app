2397fa7728758533ff06bcec194f7dfce2a9cef7
Alexis Colin <nesquimo@gmail.com>
2018-07-28 01:10:14 +0200

    Customisation android IOS de la popin index selon navigateur

--- src/components/index/install-btn/script.js
+++ src/components/index/install-btn/script.js
@@ -1,14 +1,16 @@
+import detectOS from '@/utils/detect-os';
+
 export default {
   name: 'installBtn',
   data() {
     return {
       popinDisplayed: false,
+      os: detectOS(),
     };
   },
   methods: {
     displayPopin: function $displayPopin() {
       this.popinDisplayed = this.popinDisplayed === false;
-      console.log(window.navigator);
     },
   },
 };
