4857764c26a831fc0ab74465aca2b5044eb67a27
Alexis Colin <nesquimo@gmail.com>
2018-08-01 22:05:48 +0200

    Connexion token au reload

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,10 @@ export default {
     appFooter,
   },
   created: function $created() {
+    // Connexion au reload
+    axios.defaults.headers.common.Authorization = this.$store.getters.authToken ? `Token ${this.$store.getters.authToken}` : null;
+
+    // Recup revocation de token
     axios.interceptors.response.use(undefined, err => new Promise(function $handlingAuth() {
       if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
         this.$store.dispatch('logout');
