709e66692633e7e4a7b15c18a1939f73073b921c
Alexis Colin <nesquimo@gmail.com>
2018-08-22 22:19:02 +0200

    Fix notification (affichage)

--- src/components/RouteUser/UserNotification/script.js
+++ src/components/RouteUser/UserNotification/script.js
@@ -3,7 +3,8 @@ export default {
   computed: {
     notifications: function $notification() {
       const notif = this.$store.state.callUser.userNotification;
-      return (notif && notif.length > 0) || false;
+      console.log(notif);
+      return notif.length > 0 ? notif : false;
     },
   },
 };
