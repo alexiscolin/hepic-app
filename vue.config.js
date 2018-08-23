6b83c059111045a53f358e26b2155d841f6fd3ee
Alexis Colin <nesquimo@gmail.com>
2018-08-23 15:15:27 +0200

    Fallback PWA

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
     themeColor: '#ec6730',
     msTileColor: '#FFFFFF',
     name: 'Hepic',
+    navigateFallback: '/index.html',
     workboxOptions: {
       runtimeCaching: [
         {
